c56d3073429bdef42b3cb2aecb69c8727dda6bd2
Farmin Farzin <f.farzin@digitalattitude.com>
2019-06-05 18:45:26 +0200

    :arrow_down: downgrade adn add multiple pages

---
+++ deck.js
@@ -0,0 +1,9 @@
+export { dark as theme } from "mdx-deck/themes";
+
+import one from "./pages/1.mdx";
+import two from "./pages/2.mdx";
+import three from "./pages/3.mdx";
+import four from "./pages/4.mdx";
+import five from "./pages/5.mdx";
+
+export default [...one, ...two, ...three, ...four, ...five];
