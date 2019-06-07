2706eda7636df8cb90fc09826c17a702a69ec2ca
farmin <farmin.f@gmail.com>
2019-06-07 08:58:53 +0200

    :sparkles: adding code surfer

--- deck.js
+++ deck.js
@@ -1,11 +1,12 @@
 
 export { dark as theme } from "mdx-deck/themes";
 
-import title from "./pages/title.mdx"
+import title from "./pages/title.mdx";
+import code from "./pages/code.mdx";
 import one from "./pages/1.mdx";
 import two from "./pages/2.mdx";
 import three from "./pages/3.mdx";
 import four from "./pages/4.mdx";
 import five from "./pages/5.mdx";
 
-export default [title ,...one, ...two, ...three, ...four, ...five];
+export default [...title ,...code, ...one, ...two, ...three, ...four, ...five];
