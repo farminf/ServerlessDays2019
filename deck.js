22036f80776c1f363e06850893afe1cd3c72b2c6
farmin <farmin.f@gmail.com>
2019-06-07 08:53:14 +0200

    :sparkles: adding title page

--- deck.js
+++ deck.js
@@ -1,9 +1,11 @@
+
 export { dark as theme } from "mdx-deck/themes";
 
+import title from "./pages/title.mdx"
 import one from "./pages/1.mdx";
 import two from "./pages/2.mdx";
 import three from "./pages/3.mdx";
 import four from "./pages/4.mdx";
 import five from "./pages/5.mdx";
 
-export default [...one, ...two, ...three, ...four, ...five];
+export default [title ,...one, ...two, ...three, ...four, ...five];
