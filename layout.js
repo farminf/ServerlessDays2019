d6e8989e6d0b4ce627baa76706624232fa44541c
farmin <farmin.f@gmail.com>
2019-06-11 20:51:36 +0200

    :construction:

--- layout.js
+++ layout.js
@@ -1,11 +1,22 @@
 import React from 'react'
 
 export default ({ children }) =>
+  <React.Fragment>
   <div
     style={{
       width: '100vw',
       height: '100vw',
-      backgroundColor: 'tomato'
+      backgroundColor: 'black'
     }}>
     {children}
   </div>
+  <div 
+  style={{
+    width: '100%',
+    height: '10%',
+    backgroundColor: 'tomato'
+  }}
+  >
+    <p>@farminfarzin</p>
+  </div>
+  </React.Fragment>
