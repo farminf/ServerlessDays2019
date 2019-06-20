35a84ddc97f325f0c7573b8b76d78f9e98f78e2f
Farmin Farzin <f.farzin@digitalattitude.com>
2019-06-20 16:04:17 +0200

    :construction: add theme and add images

--- theme.js
+++ theme.js
@@ -1,26 +1,32 @@
-import React from "react";
-import { white } from "ansi-colors";
-
-const Provider = props => {
-  const {  index, mode, step } = props;
-  return (
-    <div>
-      {props.children}
-      <div
-        css={{
-          position: "fixed",
-          right: 0,
-          bottom: 0,
-          margin: 16,
-          color: "white"
-        }}
-      >
-         index: {index}, mode: {mode}, step: {step}
-      </div>
-    </div>
-  );
-};
+// example theme.js
+import { dark as theme } from 'mdx-deck/themes'
 
 export default {
-  Provider
-};
+  ...theme,
+  // font: 'Futura, sans-serif',
+  h1: {
+    // textTransform: 'uppercase',
+    fontWeight: 700
+  },
+
+  img: {
+    maxWidth: '100%'
+  },
+  li: {
+    paddingBottom: '20px',
+  },
+  ul: {
+    
+    listStyle: 'none',
+    paddingLeft: '20px',
+    display: 'inline-block',
+  },
+  // colors: {
+  //   text: '#6AD798',
+  //   background: 'rgb(1, 22, 39)',
+  //   link: '#fff',
+  //   pre: '#fff',
+  //   preBackground: '#051626',
+  //   code: '#fff'
+  // }
+}
